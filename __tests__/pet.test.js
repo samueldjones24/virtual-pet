3411335e4e89428b1e56b7dfca6beaaf49485c85
Sam Jones <samueldjones24@gmail.com>
2019-02-23 16:11:41 +0000

    Added method to increment pet's age

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -9,4 +9,17 @@ describe('constructor', () => {
         const pet = new Pet('Fido');
         expect(pet.name).toEqual('Fido');
     });
+    it('has an initial age of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0);
+    });
+
+describe('growUp', () => {
+
+    it('increments the age by 1', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    });
+    });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,18 @@
 const pet = {
     name: 'Fido',
+    age: 0,
+    growUp: function() {
+        this.age += 1;
+    }
 }
 
 function Pet(name) {
     this.name = name;
+    this.age = 0;
+}
+
+Pet.prototype.growUp = function() {
+    this.age += 1;
 }
 
 module.exports = Pet;
