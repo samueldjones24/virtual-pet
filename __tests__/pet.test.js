6309d1110689f76da9eab80fd59a61df7467104b
Sam Jones <samueldjones24@gmail.com>
2019-02-24 13:57:16 +0000

    Adapted function to include effects of getting older/unhealthier

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -13,6 +13,14 @@ describe('constructor', () => {
         const pet = new Pet('Fido');
         expect(pet.age).toEqual(0);
     });
+    it('has an initial hunger of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toEqual(0);
+    })
+    it('has an initial fitness of 10', () => {
+        const pet = new Pet('Fido');
+        expect(pet.fitness).toEqual(10);
+    })
 
 describe('growUp', () => {
 
@@ -21,5 +29,15 @@ describe('growUp', () => {
         pet.growUp();
         expect(pet.age).toEqual(1);
     });
+    it('increments the hunger by 5', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
     });
+    it('decreases the fitness by 3', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
+    });
+});
 });
--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,8 @@
 const pet = {
     name: 'Fido',
     age: 0,
+    hunger: 0,
+    fitness: 10,
     growUp: function() {
         this.age += 1;
     }
@@ -9,10 +11,14 @@ const pet = {
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
 }
 
 Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += 5; 
+    this.fitness -= 3;
 }
 
 module.exports = Pet;
